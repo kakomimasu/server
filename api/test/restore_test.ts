c0ad72632386562606619159810243f1c9a60b37
Suzu Tomo <kamekyame@outlook.com>
2021-09-19 23:50:54 +0900

    Fix import name bug

--- api/test/restore_test.ts
+++ api/test/restore_test.ts
@@ -1,8 +1,8 @@
-import { assertEquals, kkmm } from "../../deps.ts";
+import { assertEquals, Core } from "../../deps.ts";
 import { ExpGame } from "../parts/expKakomimasu.ts";
 
 Deno.test("restore ExpGame class", () => {
-  const board = new kkmm.Board(2, 2, [1, 2, 3, 4], 1, 10, 1, 2);
+  const board = new Core.Board(2, 2, [1, 2, 3, 4], 1, 10, 1, 2);
   const game = new ExpGame(board);
   const restoredGame = ExpGame.restore(game.toLogJSON());
   assertEquals(game, restoredGame);
