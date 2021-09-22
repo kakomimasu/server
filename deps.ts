e610cd52e460bf22a55e3752aa0d524055d1f301
Suzu Tomo <kamekyame@outlook.com>
2021-09-23 01:27:51 +0900

    Add cors

--- deps.ts
+++ deps.ts
@@ -10,6 +10,7 @@ export * from "https://deno.land/std@0.107.0/ws/mod.ts";
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
 export * from "https://deno.land/x/servest@v1.3.4/mod.ts";
+export * from "https://deno.land/x/servest@v1.3.4/middleware/cors.ts";
 export * from "https://deno.land/x/djwt@v2.3/mod.ts";
 export * as esbuild from "https://deno.land/x/esbuild@v0.12.28/mod.js";
 export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.3.0/mod.ts";
