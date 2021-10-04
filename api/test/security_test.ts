98ede00e2b679431c3696598c481ca056a47c51c
Suzu Tomo <kamekyame@outlook.com>
2021-10-04 23:32:00 +0900

    Fix: security_test.ts

--- api/test/security_test.ts
+++ api/test/security_test.ts
@@ -1,32 +1,15 @@
 import { assertEquals } from "../../deps.ts";
 
-import * as util from "../util.ts";
-const resolve = util.pathResolver(import.meta);
-
 const host = "http://localhost:8880";
 
-Deno.test("fetch img ok", async () => {
-  const path = "/img/kakomimasu-logo.png";
-  //console.log(path);
-  const res = await fetch(host + path);
-  await res.arrayBuffer();
-  //console.log("res", res);
-  assertEquals(200, res.status);
-});
-
-// 存在しないファイルにアクセスした場合にはpages/layout.htmlを返す
+// 存在しないファイルにアクセスした場合には404を返す
 Deno.test("fetch illegal failed", async () => {
   const path = "/img/.../img/kakomimasu-logo.png";
-  //console.log(path);
   const res = await fetch(host + path);
   //console.log("res", res);
-  const body = await res.text();
-  //console.log(body);
+  await res.text();
 
-  const html = Deno.readTextFileSync(resolve("../../pages/layout.html"));
-  //console.log(html);
-
-  assertEquals(body, html);
+  assertEquals(res.status, 404);
 });
 
 const methods = {
