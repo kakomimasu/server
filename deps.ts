edc858d41056fbe3ca94f5efd7609898c1b59b5f
kamekyame <kamekyame@users.noreply.github.com>
2021-11-18 00:21:19 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 // Standard Library
-export { fromFileUrl } from "https://deno.land/std@0.114.0/path/mod.ts";
+export { fromFileUrl } from "https://deno.land/std@0.115.1/path/mod.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.114.0/testing/asserts.ts";
-export { v4 } from "https://deno.land/std@0.114.0/uuid/mod.ts";
-export * from "https://deno.land/std@0.114.0/ws/mod.ts";
+} from "https://deno.land/std@0.115.1/testing/asserts.ts";
+export { v4 } from "https://deno.land/std@0.115.1/uuid/mod.ts";
+export * from "https://deno.land/std@0.115.1/ws/mod.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
