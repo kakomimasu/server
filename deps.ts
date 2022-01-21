4c6bdb9315740accafd8450ddd673167c459ca67
kamekyame <kamekyame@users.noreply.github.com>
2022-01-21 00:21:17 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Standard Library
-export { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.121.0/encoding/yaml.ts";
-export * as Colors from "https://deno.land/std@0.121.0/fmt/colors.ts";
+export { fromFileUrl } from "https://deno.land/std@0.122.0/path/mod.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.122.0/encoding/yaml.ts";
+export * as Colors from "https://deno.land/std@0.122.0/fmt/colors.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
