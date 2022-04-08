2eebbb91bdac5a04559d953d4b114e19fd3deeb8
kamekyame <kamekyame@users.noreply.github.com>
2022-04-08 00:34:24 +0000

    build(deps): Bump deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Standard Library
-export { fromFileUrl } from "https://deno.land/std@0.132.0/path/mod.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.132.0/encoding/yaml.ts";
-export * as Colors from "https://deno.land/std@0.132.0/fmt/colors.ts";
+export { fromFileUrl } from "https://deno.land/std@0.134.0/path/mod.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.134.0/encoding/yaml.ts";
+export * as Colors from "https://deno.land/std@0.134.0/fmt/colors.ts";
 
 // Third Party Modules
 export * from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
